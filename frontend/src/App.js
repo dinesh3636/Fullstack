e1f21311a2e1ed8609b32b9afb3fa19a2723e1b4
johnjehiel <jnjehiel@gmail.com>
2024-01-10 00:01:17 +0530

    changed App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <h1>Hi ksrthik sir</h1>
       <h1>Hi merin sir</h1>
+      <h1>Hi ksrthik sir</h1>
+      <h1>Hi merin sir</h1>
     </div>
   );
 }
